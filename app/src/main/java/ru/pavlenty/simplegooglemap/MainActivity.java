d0fb9ec9b6cd053846b4cf7dbb22eb443e8d7b68
79670 <alexanderisaev@bk.ru>
2021-11-28 22:22:51 +0300

    add

--- app/src/main/java/ru/pavlenty/simplegooglemap/MainActivity.java
+++ app/src/main/java/ru/pavlenty/simplegooglemap/MainActivity.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity  implements OnMapReadyCallback {
-
+    private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +23,10 @@ public class MainActivity extends AppCompatActivity  implements OnMapReadyCallba
 
     @Override
     public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        map.addMarker(new MarkerOptions().position(new LatLng(55, 55)).title("Marker1"));
+        map.addMarker(new MarkerOptions().position(new LatLng(20, 20)).title("Marker2"));
+        map.addMarker(new MarkerOptions().position(new LatLng(-10, 55)).title("Marker1"));
+        map.addMarker(new MarkerOptions().position(new LatLng(55, -10)).title("Marker1"));
+        map.addMarker(new MarkerOptions().position(new LatLng(30, 55)).title("Marker1"));
     }
 }
